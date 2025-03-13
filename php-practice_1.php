19b2e8b5b6a68c0174b6d46b0e9ca1ef773065e2
Yamasaki0210 <akashiba0302@gmail.com>
2025-03-13 20:51:32 +0900

    課題提出

--- php-practice_1.php
+++ php-practice_1.php
@@ -1,41 +1,110 @@
 <?php
 // Q1 変数と文字列
-
+$name='山崎';
+echo  "私の名前は「 $name 」です。"."\n";
 
 // Q2 四則演算
-
+$num= 5*4;
+echo $num."\n";
+echo $num/2 ."\n";
 
 // Q3 日付操作
-
+echo date('現在時刻はY年m月d日 H時i分s秒です')."\n";
 
 // Q4 条件分岐-1 if文
-
+ $device='mac';
+ if ($device==='windows'||$device==='mac'){
+       echo "使用OSは $device です。"."\n";
+}
+else { 
+    echo 'どちらでもありません。'."\n";
+};
 
 // Q5 条件分岐-2 三項演算子
-
+$age =20;
+$userage = ($age < 18) ? '未成年です。' : '成人です。'."\n" ;
+echo $userage;
 
 // Q6 配列
+$kanto = ['東京都','神奈川県','栃木県','千葉県','埼玉県','群馬県','茨城県','愛知県','大阪府'];
+echo ($kanto[2]).'と'.($kanto[3]).'は関東地方の都道府県です。'."\n";
+$kencho=[
+    $kanto[0]=>'新宿区',
+    $kanto[1]=>'横浜市',
+    $kanto[2]=>'宇都宮市',
+    $kanto[3]=>'千葉市',
+    $kanto[4]=>'さいたま市',
+    $kanto[5]=>'前橋市',
+    $kanto[6]=>'水戸市',
+    $kanto[7]=>'名古屋市',
+    $kanto[8]=>'大阪市',
+];
 
-
-// Q7 連想配列-1
-
+foreach ($kencho as $key=>$value){
+echo $value."\n";
+}
 
 // Q8 連想配列-2
-
+foreach ($kencho as $key=>$value){
+if($key===$kanto[4]){
+echo $key.'の県庁所在地は、'.$value.'です。'."\n";
+}}
 
 // Q9 連想配列-3
-
+foreach ($kencho as $key=>$value){
+    if($key===$kanto[7]||$key===$kanto[8]){
+        echo $key.'は関東地方ではありあせん'."\n";
+    }
+    else{
+        echo $key.'の県庁所在地は、'.$value.'です。'."\n";
+    }
+}
 
 // Q10 関数-1
-
+function hello($name){
+    echo $name.'さん、こんにちは。'."\n";
+}
+hello('金谷');
+hello('安藤');
 
 // Q11 関数-2
-
+function calcTaxInPrice($price){
+    $taxInPrice=$price*1.1;
+    echo $price.'円の商品の税込価格は'.$taxInPrice.'です。'."\n";
+}
+calcTaxInPrice(1000);
 
 // Q12 関数とif文
-
+function distinguishNum($num){
+    if($num%2===0){
+        echo $num.'は偶数です。'."\n";}
+        else{
+            echo $num.'は奇数です。'."\n";
+        }
+    }
+distinguishNum(11);
+distinguishNum(24);
 
 // Q13 関数とswitch文
+function evaluateGrade($score){
+    switch($score){
+        case'A':
+        case'B':
+            echo '合格です。'."\n";
+        break;
+        case'C':
+            echo'合格ですが追加課題があります。'."\n";
+        break;
+        case'D':
+            echo'不合格です。'."\n";
+        break;
+        default:
+            echo'判定不明です。講師に問い合わせてください。'."\n";
+        break;
+    }
+}
 
+evaluateGrade('C');
+evaluateGrade('F');
 
 ?>
--- php-practice_2.php
+++ php-practice_2.php
@@ -1,15 +1,93 @@
 <?php
 // Q1 tic-tac問題
-
+$x=4;
+$y=5;
+$x=$x*$y;
+for($i=0;$i<=100;$i++){
+    if($i%$x===0){
+        echo 'tic-tac'."\n";
+    }
+    else if($i%$x===0){
+        echo 'tic'."\n";
+    }
+    else if($i%$y===0){
+        echo 'tac'."\n";
+    }
+    else{
+        echo $i."\n";
+    }
+}
 
 // Q2 多次元連想配列
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+];
+//問題1
+echo $personalInfos[1]['name'].'の電話番号は'.$personalInfos[1]['tel'].'です。'."\n";
 
+//問題2
+foreach($personalInfos as $key=>$value){
+    
+    echo $key+1 .'番目の'.$value['name'].'のメールアドレスは'.$value['mail'].'で、電話番号は'.$value['tel'].'です。'."\n"
+    ;
+}
+
+//問題3
+$ageList = [25, 30, 18];
+foreach($personalInfos as $key=>$value){
+    $personalInfos[$key]['age']=[$ageList[$key]];
+}
+var_dump($personalInfos);
 
 // Q3 オブジェクト-1
+class Student
+{
+    public $studentId;
+    public $studentName;
 
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend($gengo)
+    {
+        //echo '授業に出席しました。';
+        echo $this->studentName .'は'.$gengo.'の授業に参加しました。学籍番号：'.$this->studentId.'です。'."\n" ;
+    }
+}
+
+$gakuseki =new student(120,'山田');
+echo '学籍番号'.$gakuseki->studentId .'番の生徒は'.$gakuseki->studentName.'です。'."\n";
 
 // Q4 オブジェクト-2
-
+$yamada = new Student(120, '山田');
+$yamada->attend('PHP');
 
 // Q5 定義済みクラス
+//問題1
+$date =new DateTime();
+$date->modify("-1 months");
+echo $date-> format('Y-m-d');
+//問題2
+$mae =new DateTime('1992-04-25');
+$sa = $date-> diff($mae);
+echo $sa-> format('あの日から%a日経過しました。');
+
+
 ?>
